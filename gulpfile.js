cdf66762ba5cf323cd6ca7d7bc847bcda21ea3eb
Tyler Schwartz <schwartz.tyler+github@gmail.com>
2014-02-17 19:58:46 -0800

    making the gulpfile a little leaner

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,25 @@ var tinylr = require('tiny-lr');
 var server = tinylr();
 
 // Run default tasks
-var TaskDefault = require('./tasks/default');
-var taskDefault = new TaskDefault(gulp);
+require('./tasks/default')(gulp);
 
 // Run tasks on watch
-var TaskWatch = require('./tasks/watch');
-var taskWatch = new TaskWatch(gulp, plugins, server);
+require('./tasks/watch')(gulp, plugins, server);
 
 // Run clean tasks
-var TaskClean = require('./tasks/clean');
-var taskClean = new TaskClean(gulp, plugins);
+require('./tasks/clean')(gulp, plugins);
 
 // Run tasks on Gulp
-var TaskGulp = require('./tasks/gulp');
-var taskGulp = new TaskGulp(gulp, plugins);
+require('./tasks/gulp')(gulp, plugins);
 
 // Run tasks on HTML
-var TaskHtml = require('./tasks/html');
-var taskHtml = new TaskHtml(gulp, plugins, server);
+require('./tasks/html')(gulp, plugins, server);
 
 // Run tasks on JavaScript
-var TaskJavaScript = require('./tasks/javascript');
-var taskJavaScript = new TaskJavaScript(gulp, plugins, server);
+require('./tasks/javascript')(gulp, plugins, server);
 
 // Run tasks on SCSS
-var TaskScss = require('./tasks/scss');
-var taskScss = new TaskScss(gulp, plugins, server);
+require('./tasks/scss')(gulp, plugins, server);
 
 // Run tasks on images
-var TaskImages = require('./tasks/images');
-var taskImages = new TaskImages(gulp, plugins, server);
+require('./tasks/images')(gulp, plugins, server);
